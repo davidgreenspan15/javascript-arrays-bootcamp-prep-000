a5fdbd5e767cd37b1dd565ee7044366a161e75b9
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 12:24:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,8 +3,8 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"]
 
 
 function addElementToBeginningOfArray (array,element){
-var temp = [array]
-temp.unshift(element)
+var temp = [`${array}`]
+temp.unshift(`${element}`)
     return temp }
 
 
