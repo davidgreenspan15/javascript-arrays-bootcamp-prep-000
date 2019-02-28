4c1a69d78a8e3fc349c7dd143fa23f987208deb1
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 13:28:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,11 +12,11 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
   
 function addElementToEndOfArray(array, element) {
-  array.push(element)
+  array.push (element)
   return array
   
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
+  array.push (element)
   return array
 }
